f4faee52365819a655c3ddd5ab142d6c9d21f607
Paulo Eduardo Gatto <pauloegatto@bol.com.br>
2021-07-03 22:28:58 -0300

    finalização da 1 fase

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+require('dotenv').config()
 const axios = require("axios");
 
 
@@ -14,12 +14,12 @@ app.post("/dialogflow", async (request, response) => {
   
   if (queryResult.intent.displayName === "Default Welcome Intent") {
 
-    const client = await show(session);
-
-  if (!client)
-      return response.json({ followupEventInput: { name: "perguntacadastro" } });
+   // const client = await show(session);
+    return response.json({fulfillmentText: "oi vs code"})
+  //if (!client)
+     // return response.json({ followupEventInput: { name: "perguntacadastro" } });
      
-     return response.json({ followupEventInput: { name: "menusimples", parameters: {"nome":`${client.nome}`}}});
+    // return response.json({ followupEventInput: { name: "menusimples", parameters: {"nome":`${client.nome}`}}});
         // response.json({ followupEventInput: { name: "calltest", parameters: {"nome":"Visitante'"}}});
 
  }
