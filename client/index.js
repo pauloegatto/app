5ee92788fa94d8c2e0be48e4628a23b3e7f13ab0
Paulo Eduardo Gatto <pauloegatto@bol.com.br>
2021-07-10 22:53:01 -0300

    nova atualização

--- client/index.js
+++ client/index.js
@@ -21,7 +21,7 @@ async function createempresa(parameters, session) {
   
   const idUser = session.split('/')[4];
 
-  await axios.post(
+ /* await axios.post(
     process.env.URL_SHEET2,
     {
       id: idUser,
@@ -42,7 +42,7 @@ async function createempresa(parameters, session) {
         password: process.env.PASSWORD_SHEET
       }
     }
-  );
+  );*/
   console.log(parameters,empresa)
   return { parameters, empresa };
 }
