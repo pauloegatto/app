3d7dfa2cea7f3ececbf465c52a1046dd099fa91f
Paulo Eduardo Gatto <pauloegatto@bol.com.br>
2021-08-23 23:16:19 -0300

    23/08/21

--- client/index.js
+++ client/index.js
@@ -1,8 +1,13 @@
+
 const axios = require("axios");
-const { viacep, cnpjCpf } = require("../functions");
+const { viacep, cnpjCpf, geolocation, sms } = require("../functions");
 
 async function show(query) {
-  const idUser = query.split('/')[4];
+  const data = query.split('/');
+  const numero = data[data.length - 1]   
+  
+  
+  const idUser = String(numero).match(/\d+/g).join('');
   const result = await axios.get(`${process.env.URL_SHEET}/search`, {
     params: {
       id: idUser
@@ -16,32 +21,55 @@ async function show(query) {
   return result.data[0];
 }
 
-async function createempresa(parameters, session) {
+async function createempresa(parameters, session, textoCnpj) {
   console.log(parameters)
-  const empresa = await cnpjCpf(parseInt(parameters.cnpjCpf));
+  const empresa = await cnpjCpf(textoCnpj);
   
-  const idUser = session.split('/')[4];
-
+  const data = session.split('/');
+  const numero = data[data.length - 1]
+  const idUser = String(numero).match(/\d+/g).join('');
+  const cnpj = String(empresa.cnpj).match(/\d+/g).join('');
+  const tipo = String(parameters.tipo);
+  const modo = tipo.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+ // const numerosms = parameters.whats
+    //await sms(cnpj, numerosms)
+  let texto2Ajustado;
+      const parte3 = String(cnpj).match(/\d+/g).join('');
+      const parte4 = `${parte3.slice(6,7)}${parte3.slice(3,4)}${parte3.slice(1,2)}${parte3.slice(7,8)}`;
+      texto2Ajustado = `${parte4}`;
+      const senha = texto2Ajustado
+  
+  
   await axios.post(
-    process.env.URL_SHEET2,
+    process.env.URL_SHEET3,
     {
       id: idUser,
-      nomeFantasia: empresa.fantasia,
+      nomeFantasia: parameters.fantasias.toUpperCase(),
       razaoSocial:  empresa.nome,
-      cnpj:  empresa.cnpj,
-     // telefoneWhats:  empresa.,
+      cnpj:  cnpj,
+      whats:  parameters.whats,
       telefone:  empresa.telefone,
       cep:  empresa.cep,
       localidade:  empresa.municipio,
       uf:  empresa.uf,
       rua:  empresa.logradouro,
       numero:  empresa.numero,
-      bairro: empresa.bairro
+      bairro: empresa.bairro,
+      tipo: tipo,
+      horario: parameters.horario,
+      atividade: parameters.atividade,
+      responsavel: parameters.responsavel,
+      login: senha,
+      facebook: parameters.facebook,
+      instagram: parameters.instagram,
+      site: parameters.site,
+      comochegar: parameters.comochegar,
+      email: parameters.email
     },
     {
       auth: {
-        username: process.env.LOGIN_SHEET,
-        password: process.env.PASSWORD_SHEET
+        username: process.env.LOGIN_SHEET3,
+        password: process.env.PASSWORD_SHEET3
       }
     }
   );
@@ -54,19 +82,36 @@ async function createempresa(parameters, session) {
 async function create(parameters, session) {
   const andress = await viacep(parameters.cep);
   
-  const idUser = session.split('/')[4];
-
+  
+//   const numero1 = parameters.numero
+//   const rua1 = andress.logradouro
+//   const cidade1 = andress.localidade
+//   const uf1 = andress.uf
+//   const geo = await geolocation(numero1, rua1, cidade1 , uf1)
+//   console.log("teste geo create")
+//   console.log(geo)
+//   console.log(geo.results.geometry.location.lat)
+//   console.log(geo.location.lat)
+  
+  
+  const data = session.split('/');
+  const numero = data[data.length - 1]
+  const idUser = String(numero).match(/\d+/g).join('');
+  const primeironome = parameters.nome.split(' ')[0];
   await axios.post(
     process.env.URL_SHEET,
     {
       id: idUser,
-      nome: parameters.nome,
-      telefone: parameters.telefone,
+      nome: primeironome,
+      nomecompleto: parameters.nome.toUpperCase(),
+      telefone: parameters.telefone.toUpperCase(),
       cep: parameters.cep,
       localidade: andress.localidade,
       uf: andress.uf,
       rua: andress.logradouro,
-      numero: parameters.numero
+      numero: parameters.numero,
+      bairro: andress.bairro,
+      apoiolocal: parameters.apoiolocal
     },
     {
       auth: {
@@ -79,14 +124,17 @@ async function create(parameters, session) {
 }
 
 
-async function updateName(nome, session) {  
+async function update(parameters, session) {  
   
-    const idUser = session.split('/')[4];
-
+  
+  const data = session.split('/');
+  const numero = data[data.length - 1]
+  const idUser = String(numero).match(/\d+/g).join('');
   await axios.patch(
     `${process.env.URL_SHEET}/id/${idUser}`,
     {
-      nome: nome, 
+      ...parameters,
+      
     },
     {
       auth: {
@@ -96,7 +144,7 @@ async function updateName(nome, session) {
     }
   );
   
-  return nome;
+  return {...parameters}
 }
 
 async function deleteAll() {  
@@ -115,8 +163,176 @@ async function deleteAll() {
 }
 
 
+async function showpj(parameters) {
+
+const textoCnpj = String(parameters);
+  console.log("showpj")
+  console.log(textoCnpj)
+//    let textoAjustado 
+ 
+//         const parte1 = parameters.slice(0,2);
+//         const parte2 = parameters.slice(2,5);
+//         const parte3 = parameters.slice(5,8);
+//         const parte4 = parameters.slice(8,12);
+//         const parte5 = parameters.slice(12,14);
+     
+//         textoAjustado = `${parte1}.${parte2}.${parte3}/${parte4}-${parte5}`
+//         const cnpj = await textoAjustado
+
+  const result = await axios.get(`${process.env.URL_SHEET3}/search`, {
+    params: {
+      cnpj: textoCnpj
+    },
+    auth: {
+      username: process.env.LOGIN_SHEET3,
+      password: process.env.PASSWORD_SHEET3
+    }
+  });
+
+ 
+  return result.data[0];
+}
+  
+// const result = await axios.get(`${process.env.URL_SHEET2}`,{
+//     auth: {
+//       username: process.env.LOGIN_SHEET,
+//       password: process.env.PASSWORD_SHEET
+//     }
+//   });
+
+//   return result.data.filter(search => search.cnpjCpf === query);
+// }
 
 
+async function pesquisa (parameters) {
+   const cepnovo = String(parameters.cep)
+      .match(/\d+/g)
+      .join("");
+  
+  let textoAjustado 
+  
+
+        const parte1 = cepnovo.slice(0,5);
+        const parte2 = cepnovo.slice(5,8);
+      
+
+        textoAjustado = `${parte1}-${parte2}`
+        const cep = textoAjustado
+  
+  console.log("teste pesquisa cep")
+      console.log(parameters.cep)
+ 
+   console.log(cep)
+  
+  const andress = await viacep(cep);
+  
+  const atividade = String(parameters.atividade).normalize("NFD").replace(/[^a-zA-Zs]/g, "").toUpperCase();
+ console.log("pesquisa")
+   console.log(atividade)
+
+  const result = await axios.get(`${process.env.URL_SHEET3}`,{
+    auth: {
+      username: process.env.LOGIN_SHEET3,
+      password: process.env.PASSWORD_SHEET3
+    }
+  });
+
+  //return result.data.filter(search => search.atividade === atividade);
+  
+  return result.data.filter(search => search.localidade === andress.localidade.toUpperCase()).filter(search => search.atividade === atividade);
+  console.log(andress)
+ // console.log(lista)
+  //return lista.data.filter(search => search.atividade === atividade);
+}
+async function pesquisasimples (parameters) {
+   //query
+  const andress = await viacep(parameters.cep);
+console
+  const result = await axios.get(`${process.env.URL_SHEET3}`,{
+    auth: {
+      username: process.env.LOGIN_SHEET3,
+      password: process.env.PASSWORD_SHEET3
+    }
+  });
+
+  return result.data.filter(search => search.localidade === andress.localidade.toUpperCase());
+  
+  
+  console.log(andress)
+ // console.log(lista)
+  //return lista.data.filter(search => search.atividade === atividade);
+}
+
+  function arrumaCelular (session) {
+   const data = session.split("/");
+    const numero = data[data.length - 1];
+    const telefone = String(numero)
+      .match(/\d+/g)
+      .join("");
+  
+  let textoAjustado 
+  
+
+        const parte1 = telefone.slice(2,4);
+        const parte2 = telefone.slice(4,8);
+        const parte3 = telefone.slice(8,12);
+
+        textoAjustado = `0${parte1} 9${parte2}-${parte3}`
+        const celular = textoAjustado
+        
+        return {celular};
+    }
 
 
-module.exports = { show, create, updateName, deleteAll, createempresa };
+async function createpf(parameters, session, textoCnpj, client) {
+  console.log(parameters)
+ // const empresa = await cnpjCpf(textoCnpj);
+
+  const data = session.split('/');
+  const numero = data[data.length - 1]
+  const idUser = String(numero).match(/\d+/g).join('');
+  const tipo = String(parameters.tipo);
+  const modo = tipo.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+
+  let texto2Ajustado;
+      const parte3 = String(textoCnpj).match(/\d+/g).join('');
+      const parte4 = `${parte3.slice(6,7)}${parte3.slice(3,4)}${parte3.slice(1,2)}${parte3.slice(7,8)}`;
+      texto2Ajustado = `${parte4}`;
+      const senha = texto2Ajustado
+      
+  console.log(tipo)
+  console.log(textoCnpj)
+  await axios.post(
+    process.env.URL_SHEET3,
+    {
+      id: idUser,
+      nomeFantasia: parameters.nome,
+      razaoSocial:  client.nomecompleto,
+      cnpj:  textoCnpj,
+      whats:  client.telefone,
+      telefone:  client.telefone,
+      cep:  client.cep,
+      localidade:  client.localidade.toUpperCase(),
+      uf:  client.uf,
+      rua:  client.rua,
+      numero:  client.numero,
+      bairro: client.bairro,
+      tipo: tipo,
+      horario: parameters.horario,
+      atividade: parameters.servicos,
+      responsavel: client.nome,
+      login: senha
+    },
+    {
+      auth: {
+        username: process.env.LOGIN_SHEET3,
+        password: process.env.PASSWORD_SHEET3
+      }
+    }
+  );
+  //console.log(parameters,empresa)
+  return { parameters};
+}
+
+
+module.exports = { show, createpf, create, update, deleteAll, createempresa, showpj , pesquisa, arrumaCelular, pesquisasimples};
--- functions/index.js
+++ functions/index.js
@@ -16,11 +16,84 @@ async function cnpjCpf(cnpj) {
   return result.data;
 }
 
-/*(async()=> {
+function TestaCPF(strCPF) {
+    let Soma;
+    let Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (let i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
+
+async function geolocation (numero1, rua1, cidade1 , uf1 ) {
+
+  //https://maps.googleapis.com/maps/api/geocode/json?address=340+Avenida+Pirapo,+Apucarana,+PR&key=AIzaSyBEvaFSyLB4d_HD2ylCYX3ZqSISAg8cKwQ
+
+const rua =  rua1.replace(" ", "+");
+const cidade =  cidade1.replace(" ", "+");
+console.log("teste geolocation")
+console.log(rua)
+console.log(cidade)
+console.log(numero1)
+console.log(uf1)
+console.log(await axios.get(`${process.env.URL_GEO}${numero1}+${rua},+${cidade},+${uf1}&key=${process.env.KEY_GEO}`))
+
+const result = await axios.get(`${process.env.URL_GEO}${numero1}+${rua},+${cidade},+${uf1}&key=${process.env.KEY_GEO}`);
+console.log(result.data)
+
+return result.data;
+}
+
+
+
+async function sms(cnpj, numerosms) {
+ console.log("teste sms")
+   let textoAjustado 
   
-  const resposta = await cnpjCpf(23858717000174)
-  console.log(resposta.fantasia)})()*/
+        const parte1= String(numerosms).match(/\d+/g).join('');
+        const parte2 = parte1.slice(1,12);       
+        textoAjustado = `55${parte2}`
+        const celular = textoAjustado
+  console.log(celular)
+   let texto2Ajustado;
+
+       console.log(cnpj)
+        const parte3 = String(cnpj).match(/\d+/g).join('');
+        console.log(parte3)
+      const parte4 = `${parte3.slice(6,7)}${parte3.slice(3,4)}${parte3.slice(1,2)}${parte3.slice(7,8)}`;
+        console.log(parte4)
+  texto2Ajustado = `${parte4}`;
+        const senha = texto2Ajustado
+ console.log(senha)
+ console.log(`senha:${senha}`)
+ 
+  
+ const texto1 = `Cadastro%20102paraWhats%20feito%20com%20sucesso!%20Sua%20senha%20e:%20${senha}.%20Para%20modificar%20seu%20cadastro%20acesso%20o%20link:%20https://wa.me/554391169015%20e%20digite%20CADASTRO.%20Obrigado%20:)`;
+  const texto = texto1
+   const URL_SMS = "http://smsmarketing.smslegal.com.br/index.php?app=webservices&u="
+ 
+  console.log(`${URL_SMS}${process.env.LOGIN_SMS}&p=${process.env.SENHA_SMS}&ta=pv&to=${celular}&msg=${texto}`)
+ const result =  await axios.get(`${URL_SMS}${process.env.LOGIN_SMS}&p=${process.env.SENHA_SMS}&ta=pv&to=${celular}&msg=${texto}`)
+
+ const retorno = result.data
+  return { retorno, senha };
+}
 
 
 
-module.exports = { viacep, cnpjCpf };
+
+
+module.exports = { viacep, cnpjCpf, TestaCPF, geolocation, sms };
