559e4c94bbb7fd1f2fc203b7ebdf7c716fef604d
pauloegatto <86896990+pauloegatto@users.noreply.github.com>
2021-09-15 20:35:41 -0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -42,19 +42,29 @@ async function geolocation (numero1, rua1, cidade1 , uf1 ) {
 
   //https://maps.googleapis.com/maps/api/geocode/json?address=340+Avenida+Pirapo,+Apucarana,+PR&key=AIzaSyBEvaFSyLB4d_HD2ylCYX3ZqSISAg8cKwQ
 
-const rua =  rua1.replace(" ", "+");
-const cidade =  cidade1.replace(" ", "+");
+const ruas = rua1.normalize("NFD").replace(/[^a-zA-Zs]/g, "")
+const rua =  ruas.replace(" ", "+");
+const cidades = cidade1.normalize("NFD").replace(/[^a-zA-Zs]/g, "")
+const cidade =  cidades.replace(" ", "+");
 console.log("teste geolocation")
-console.log(rua)
-console.log(cidade)
 console.log(numero1)
-console.log(uf1)
-console.log(await axios.get(`${process.env.URL_GEO}${numero1}+${rua},+${cidade},+${uf1}&key=${process.env.KEY_GEO}`))
+  console.log(rua1)
+   console.log(cidade1)
+   console.log(uf1)
+   console.log(rua)
+  console.log(cidade)
+ const result = await axios.get(`${process.env.URL_GEO}${numero1}+${rua},+${cidade},+${uf1}&key=${process.env.KEY_GEO}`);
+    const {
+        lat,
+        lng
+    } = result.data.results[0].geometry.location;
+console.log(lat)
+  console.log(lng)
+    return {
+        lat,
+        lng
+    };
 
-const result = await axios.get(`${process.env.URL_GEO}${numero1}+${rua},+${cidade},+${uf1}&key=${process.env.KEY_GEO}`);
-console.log(result.data)
-
-return result.data;
 }
 
 
@@ -133,4 +143,49 @@ async function sms2(numerosms) {
 }
 
 
-module.exports = { viacep, cnpjCpf, TestaCPF, geolocation, sms, sms2 };
+
+function calcGeo(lat11, lng11, lat22, lng22) {
+    Number.prototype.toRad = function () {
+        return this * Math.PI / 180;
+    }
+  console.log("calcGeo função1")
+ console.log(lat11)
+  console.log(lng11)
+  console.log(lat22)
+  console.log(lng22)
+  
+    // let latitude1 = Number(lat1);
+    // let longitude1 = Number(lng1);
+    // let latitude2 = Number(lat2);
+    // let longitude2 = Number(lng2);
+
+let lat2 = lat22;
+let lon2 = lng22;
+let lat1 = lat11;
+let lon1 = lng11;
+  console.log("calcGeo função2")
+ 
+  console.log(lat2)
+  console.log(lon2)
+  console.log(lat1)
+  console.log(lon1)
+let R = 6371; // km 
+//has a problem with the .toRad() method below.
+let x1 = lat2-lat1;
+let dLat = x1.toRad();  
+let x2 = lon2-lon1;
+let dLon = x2.toRad();  
+let a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2);  
+let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+let d = R * c; 
+
+    return d;
+}
+
+
+
+
+
+module.exports = { viacep, cnpjCpf, TestaCPF, geolocation, sms, sms2, calcGeo };
